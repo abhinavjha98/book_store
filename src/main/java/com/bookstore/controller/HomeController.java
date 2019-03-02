147225082b1daeee345cbb6a4b93453493d7fb05
Abhinav <abhinavjha98ald@gmail.com>
2019-03-02 21:34:57 +0530

    Added the carousel

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,5 +1,11 @@
 package com.bookstore.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class HomeController {
-
+	@RequestMapping("/")
+	public String index() {
+		return "index"; 
+	}
 }
